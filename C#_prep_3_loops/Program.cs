de3f63c2cadb1fb2fda6d9991d409738559efe18
gasolina90 <moralerick90@gmail.com>
2023-01-10 15:52:09 -0700

    modified file

--- C#_prep_3_loops/Program.cs
+++ C#_prep_3_loops/Program.cs
@@ -1,23 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-// int[] myList = new int[10];
-// numArray = null;
-
-// for (int i = 0; i < 10; i++ ){
-//     Random rng = new Random();
-//     int num = rng.Next(1,11);
-
-//     List<int> myList = new List<int>();
-//     myList.Append(num);
-
-//     // return myList;
-//     Console.WriteLine(List<int>,myList);
-
-//     // numArray = numArray.Append(num).ToArray();
-
-// }
-
 /*********************************************************
 *
 *   Practice with for, foreach, while, do-while loops
@@ -27,49 +9,54 @@
 using System;
 using System.Collections.Generic;
 
-// for loop
-for (int i = 0; i < 50; i++){
-    Console.WriteLine(i);
-}
+// // for loop
+// for (int i = 0; i < 50; i++){
+//     Console.WriteLine(i);
+// }
 
-// for-each loop
-string cars = "Toyota Prius";
+// // for-each loop
+// string cars = "Toyota Prius";
 
-foreach (char letter in cars){
-    Console.WriteLine(letter);
-}
+// foreach (char letter in cars){
+//     Console.WriteLine(letter);
+// }
 
-// While loop
-string input = null;
-while (input != "no"){
-    Console.WriteLine("Stay in the loop? ");
-    input = Console.ReadLine();
-}
+// // While loop
+// string input = null;
+// while (input != "no"){
+//     Console.WriteLine("Stay in the loop? ");
+//     input = Console.ReadLine();
+// }
 
-// Number guesser
-Random rng = new Random();
-int rand_num = rng.Next(1,11);
+// do-while loop
+string input2;
 
-Console.WriteLine("What is your guess? ");
-var guess = Console.ReadLine();
-int guess_int = Convert.ToInt32(guess);
-// Console.WriteLine(guess);
+do{
+    Console.WriteLine("Stay in the loop?");
+    input2 = Console.ReadLine();
+}while(input2 != "no");
 
+// // Number guesser
+// Random rng = new Random();
+// int rand_num = rng.Next(1,11);
 
-while (guess_int < rand_num){
-    if (guess_int < rand_num){
-        Console.WriteLine("Guess higher!");
-        break;
-    }
-    if (guess_int > rand_num){
-        Console.WriteLine("Guess lower!");
-        break;
-    }
-    if (guess_int == rand_num){
-        Console.WriteLine("You guessed Correctly! ^_^");
-        break;
-    }
-}
+// Console.WriteLine("What is your guess? ");
+// var guess = Console.ReadLine();
+// int guess_int = Convert.ToInt32(guess);
+// // Console.WriteLine(guess);
 
 
-// "=================================================================================";
+// while (guess_int < rand_num){
+//     if (guess_int < rand_num){
+//         Console.WriteLine("Guess higher!");
+//         break;
+//     }
+//     if (guess_int > rand_num){
+//         Console.WriteLine("Guess lower!");
+//         break;
+//     }
+//     if (guess_int == rand_num){
+//         Console.WriteLine("You guessed Correctly! ^_^");
+//         break;
+//     }
+// }
