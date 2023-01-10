ac7e2be35f724338e4a3e27df9424a8a0d176c70
gasolina90 <moralerick90@gmail.com>
2023-01-10 16:18:49 -0700

    added list practice dir

---
+++ C#_prep4_lists/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+*
+*   Practice creating and adding to lists, and list methods
+*
+*/
+
+// Library for lists
+using System;
+using System.Collections.Generic;
+
+// Instantiate the list with List<int> nums;
+// Instantiate a variable and save the previously created list with that variable. = new List<int>();
+List<int> nums = new List<int>();
+List<string> strs = new List<string>();
+
+strs.Add("Toyota");
+strs.Add("Honda");
+strs.Add("Nissan");
+
+Console.WriteLine(strs);
