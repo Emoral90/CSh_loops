ede4ead7887482c9d0fa194dd018d7c03233575b
gasolina90 <moralerick90@gmail.com>
2023-01-10 15:43:37 -0700

    modified file

--- C#_prep_3_loops/Program.cs
+++ C#_prep_3_loops/Program.cs
@@ -28,7 +28,6 @@ using System;
 using System.Collections.Generic;
 
 // for loop
-
 for (int i = 0; i < 50; i++){
     Console.WriteLine(i);
 }
@@ -40,31 +39,37 @@ foreach (char letter in cars){
     Console.WriteLine(letter);
 }
 
-// // Number guesser
+// While loop
+string input = null;
+while (input != "no"){
+    Console.WriteLine("Stay in the loop? ");
+    input = Console.ReadLine();
+}
 
-// Random rng = new Random();
-// int rand_num = rng.Next(1,11);
+// Number guesser
+Random rng = new Random();
+int rand_num = rng.Next(1,11);
 
-// Console.WriteLine("What is your guess? ");
-// var guess = Console.ReadLine();
-// int guess_int = Convert.ToInt32(guess);
-// // Console.WriteLine(guess);
+Console.WriteLine("What is your guess? ");
+var guess = Console.ReadLine();
+int guess_int = Convert.ToInt32(guess);
+// Console.WriteLine(guess);
 
 
-// while (guess_int < rand_num){
-//     if (guess_int < rand_num){
-//         Console.WriteLine("Guess higher!");
-//         break;
-//     }
-//     if (guess_int > rand_num){
-//         Console.WriteLine("Guess lower!");
-//         break;
-//     }
-//     if (guess_int == rand_num){
-//         Console.WriteLine("You guessed Correctly! ^_^");
-//         break;
-//     }
-// }
+while (guess_int < rand_num){
+    if (guess_int < rand_num){
+        Console.WriteLine("Guess higher!");
+        break;
+    }
+    if (guess_int > rand_num){
+        Console.WriteLine("Guess lower!");
+        break;
+    }
+    if (guess_int == rand_num){
+        Console.WriteLine("You guessed Correctly! ^_^");
+        break;
+    }
+}
 
 
 // "=================================================================================";
